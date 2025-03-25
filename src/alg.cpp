3f00d7b753eadac82b5b14d37bb909f8df1d79cf
pootaatoovarenaya <lilechekT@mail.ru>
2025-03-25 22:41:37 +0300

    TTTT

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,7 +6,9 @@ int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
     for (int j = i + 1; j < len; ++j) {
-      if (arr[i] + arr[j] == value) ++count;
+      if (arr[i] + arr[j] == value) {
+        ++count;
+      }
     }
   }
   return count;
@@ -21,34 +23,48 @@ int countPairs2(int *arr, int len, int value) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
       if (arr[left] == arr[right]) {
-        count += (right - left) * (right - left + 1) / 2;
+        int n = right - left + 1;
+        count += n * (n - 1) / 2;
         break;
+      } else {
+        int current_left = arr[left];
+        int current_right = arr[right];
+        int left_count = 0;
+        int right_count = 0;
+
+        while (left < len && arr[left] == current_left) {
+          ++left;
+          ++left_count;
+        }
+
+        while (right >= 0 && arr[right] == current_right) {
+          --right;
+          ++right_count;
+        }
+
+        count += left_count * right_count;
       }
-      int left_val = arr[left];
-      int right_val = arr[right];
-      while (left < len && arr[left] == left_val) ++left;
-      while (right >= 0 && arr[right] == right_val) --right;
-      count += (left - (left_val == arr[left] ? left : left - 1)) * 
-               ((right_val == arr[right] ? right : right + 1) - right);
-    } 
-    else if (sum < value) ++left;
-    else --right;
+    } else if (sum < value) {
+      ++left;
+    } else {
+      --right;
+    }
   }
   return count;
 }
 
 int binarySearch(int *arr, int left, int right, int target) {
-  int result = -1;
+  int first = -1;
   while (left <= right) {
     int mid = left + (right - left) / 2;
-    if (arr[mid] == target) {
-      result = mid;
-      right = mid - 1; // Ищем первое вхождение
-    } 
-    else if (arr[mid] < target) left = mid + 1;
-    else right = mid - 1;
+    if (arr[mid] >= target) {
+      if (arr[mid] == target) first = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
   }
-  return result;
+  return first;
 }
 
 int countPairs3(int *arr, int len, int value) {
@@ -56,12 +72,14 @@ int countPairs3(int *arr, int len, int value) {
   for (int i = 0; i < len; ++i) {
     int target = value - arr[i];
     if (target < arr[i]) continue;
+
     int first = binarySearch(arr, i + 1, len - 1, target);
     if (first != -1) {
-      while (first <= len - 1 && arr[first] == target) {
-        ++count;
-        ++first;
+      int last = first;
+      while (last + 1 < len && arr[last + 1] == target) {
+        ++last;
       }
+      count += last - first + 1;
     }
   }
   return count;
