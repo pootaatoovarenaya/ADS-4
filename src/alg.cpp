5fc4d92fea2b0abf15c6b059951f30a2e3a868d8
pootaatoovarenaya <lilechekT@mail.ru>
2025-03-26 13:12:32 +0300

    TTTT

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,12 @@
+// Copyright 2022 Your Name
+
 #include <stdio.h>
+
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; i++) {
     if (i > 0 && arr[i] == arr[i-1]) continue;
+    
     for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
         count++;
@@ -12,30 +16,37 @@ int countPairs1(int *arr, int len, int value) {
   }
   return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
   int left = 0;
   int right = len - 1;
+  
   while (left < right) {
     int sum = arr[left] + arr[right];
+    
     if (sum == value) {
       if (arr[left] == arr[right]) {
         int n = right - left + 1;
         count += n * (n - 1) / 2;
         break;
       }
+      
       int left_val = arr[left];
       int right_val = arr[right];
       int left_count = 1;
       int right_count = 1;
+      
       while (left + 1 < right && arr[left+1] == left_val) {
         left++;
         left_count++;
       }
+      
       while (right - 1 > left && arr[right-1] == right_val) {
         right--;
         right_count++;
       }
+      
       count += left_count * right_count;
       left++;
       right--;
@@ -45,8 +56,10 @@ int countPairs2(int *arr, int len, int value) {
       right--;
     }
   }
+  
   return count;
 }
+
 int binarySearch(int *arr, int left, int right, int value) {
   while (left <= right) {
     int mid = left + (right - left) / 2;
@@ -61,12 +74,15 @@ int binarySearch(int *arr, int left, int right, int value) {
   }
   return -1;
 }
+
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len - 1; i++) {
     if (i > 0 && arr[i] == arr[i-1]) continue;
+    
     int target = value - arr[i];
     int pos = binarySearch(arr, i+1, len-1, target);
+    
     if (pos != -1) {
       int j = pos;
       while (j < len && arr[j] == target) {
